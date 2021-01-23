511472b80c02e8fc3e7f7bdb97e01903a00b4560
huiyu <hui.yu@britishcouncil.org.com>
2021-01-23 11:41:28 +0800

    add cors

--- BC.Ns.Api/App_Start/WebApiConfig.cs
+++ BC.Ns.Api/App_Start/WebApiConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BC.Ns.Api.App_Start
 {
@@ -8,9 +9,17 @@ namespace BC.Ns.Api.App_Start
         public static void Register(HttpConfiguration config)
         {
             // Web API 配置和服务
+
+            // 跨域配置
+            //config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            // Filters 配置
             FilterConfig.RegisterGlobalFilters(config.Filters);
+
+            // Swagger 配置
             SwaggerConfig.Register(config);
 
+            // 安全协议
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
             // Web API 路由
